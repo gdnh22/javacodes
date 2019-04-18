fe222c45b2ee82accf32ac20c1e34c3516bfc83d
gdnh22 <gdnh_16@163.com>
2019-04-18 17:34:45 +0800

    println

--- demo1/Demo1/src/HelloWorld.java
+++ demo1/Demo1/src/HelloWorld.java
@@ -1,9 +1,10 @@
-
+import java.text.DecimalFormat;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		DecimalFormat myFormat=new DecimalFormat();
+		myFormat.setGroupingUsed(false);
 		// TODO Auto-generated method stub
-
+System.out.println("Hello, "+"JAVA 12, current date is "+myFormat.format(20190418));
 	}
-
 }
