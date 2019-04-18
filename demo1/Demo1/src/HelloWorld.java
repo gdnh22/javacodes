d0483814a0352b76077729f9f5f38c425e63c807
gdnh22 <gdnh_16@163.com>
2019-04-19 01:02:43 +0800

    Revert "gettime"
    This reverts commit c592603c498178b626ee5bad8942a599434bc3fe.

--- demo1/Demo1/src/HelloWorld.java
+++ demo1/Demo1/src/HelloWorld.java
@@ -1,15 +1,10 @@
-import java.text.DecimalFormat;//格式化
-import java.util.GregorianCalendar;	//日期
-import java.text.SimpleDateFormat;	//格式化日期
+import java.text.DecimalFormat;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		DecimalFormat myFormat=new DecimalFormat();
 		myFormat.setGroupingUsed(false);
 		// TODO Auto-generated method stub
-		System.out.println("Hello, "+"JAVA 12, current date is "+myFormat.format(20190418));
-		GregorianCalendar cln=new GregorianCalendar();	//日期
-		SimpleDateFormat sdf=new SimpleDateFormat();
-		System.out.println("the real time is "+sdf.format(cln.getTime()));	//当前日期
+System.out.println("Hello, "+"JAVA 12, current date is "+myFormat.format(20190418));
 	}
 }
