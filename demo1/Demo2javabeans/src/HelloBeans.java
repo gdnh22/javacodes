cdb19a7efb5d035fbfb52b98f40bb28ee51238ac
gdnh22 <gdnh_16@163.com>
2019-04-19 15:29:27 +0800

    javabenas
    jsp

---
+++ demo1/Demo2javabeans/src/HelloBeans.java
@@ -0,0 +1,10 @@
+
+public class HelloBeans {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("本模块由%s负责编写","王振山");  println不支持逗号连接的字符串
+		System.out.printf("本模块由%s负责编写","王振山");
+	}
+
+}
